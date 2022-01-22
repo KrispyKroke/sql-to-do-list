fa67ebd4f79285885684815603801e334dbc4280
Jared Kroke <jared.kroke@gmail.com>
2022-01-22 16:25:28 -0600

    Set up POST route on server-side. Tested in Postman

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -3,10 +3,25 @@ const router = express.Router();
 // Connects server to database for data manipulation
 const pool = require('../modules/pool.js');
 
+
+// Server-side GET route. Queries the database to retrieve all tasks stored in it and sends them back
+// to the front-end upon successful retrieval.
 router.get('/', (req, res) => {
     let queryText = 'SELECT * FROM tasks;';
     pool.query(queryText).then((response) => {
         res.send(response.rows);
+    }).catch((error) => {
+        console.log(error);
+        res.sendStatus(500);
+    });
+});
+
+router.post('/', (req, res) => {
+    let task = req.body;
+    let queryText = `INSERT INTO tasks ("task") 
+    VALUES ($1);`;
+    pool.query(queryText, [task.task]).then(() => {
+        res.sendStatus(201);
     }).catch((error) => {
         console.log(error);
         res.sendStatus(500);
