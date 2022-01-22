69b1a24eeac603bd82b1aa4aeeb48c0eae031975
Jared Kroke <jared.kroke@gmail.com>
2022-01-22 16:05:47 -0600

    Set up routes. Ready to begin work on the back-end GET/POST routes

--- server/server.js
+++ server/server.js
@@ -3,9 +3,14 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
+const router = require('./routes/list.router.js');
+
 
 app.use(express.static('server/public'));
 app.use(express.urlencoded({extended: true}));
+
+// Sets up route to funnel everything with the url ending in /list to the list.router.js file
+app.use('/list', router);
 
 
 
