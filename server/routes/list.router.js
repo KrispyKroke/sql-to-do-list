2e417c1a1132748f6c231207ff3416f709497a21
Jared Kroke <jared.kroke@gmail.com>
2022-01-22 16:13:21 -0600

    Set up GET route on server-side. Tested in Postman

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const router = express.Router();
-
+// Connects server to database for data manipulation
 const pool = require('../modules/pool.js');
+
+router.get('/', (req, res) => {
+    let queryText = 'SELECT * FROM tasks;';
+    pool.query(queryText).then((response) => {
+        res.send(response.rows);
+    }).catch((error) => {
+        console.log(error);
+        res.sendStatus(500);
+    });
+});
 
 
 
