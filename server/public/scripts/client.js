caa6f4b8bc1406abf033015defc10a48c7ab761e
Jared Kroke <jared.kroke@gmail.com>
2022-01-23 14:11:06 -0600

    Set up client-side GET request. It is properly retrieving tasks from the back-end and displaying them on the DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,34 @@ $(onReady);
 
 function onReady() {
     console.log('JQuery is loaded.');
+    showTasks();
+}
+// Function which makes an ajax GET request to the server to retrieve all tasks stored in the database.
+function showTasks() {
+    $.ajax({
+        method: 'GET',
+        url: '/list',
+    }).then((response) => {
+        // Initializes a count variable to give each task a numeric value for ordering on the DOM
+        let count = 1;
+        $('#toDoList').empty();
+        for (task of response) {
+            // Gives each task an 'N' or 'Y' depending on completion status in the database
+            let status = 'N';
+            if (task.completionStatus === true) {
+                status = 'Y';
+            }
+            // Appends all tasks in the database to the DOM
+            $('#toDoList').append(`<tr>
+                <td>${count}</td>
+                <td>${task.task}</td>
+                <td>${status}</td>
+                <td><button class="endButton" id="completionButton" data-id="${task.id}">Mark as Complete/Incomplete</button></td>
+                <td><button class="endButton" id="deleteButton" data-id="${task.id}">Delete Task</button></td>
+            </tr>`);
+            count++;
+        }
+    }).catch((error) => {
+        alert(error);
+    });
 }
