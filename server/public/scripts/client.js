d236be8c0f20f8c8c962a48dc5522f6b2738fa1e
Jared Kroke <jared.kroke@gmail.com>
2022-01-23 14:37:55 -0600

    Incorporated an ajax POST request into the front-end. It allows the ability to add a new task to the to-do list on the DOM.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,7 +2,10 @@ $(onReady);
 
 function onReady() {
     console.log('JQuery is loaded.');
+    // Shows all tasks in the database when the application is booted up
     showTasks();
+    // Waits for a task to be submitted before initiating the addTask function
+    $('#submitTask').on('click', addTask);
 }
 // Function which makes an ajax GET request to the server to retrieve all tasks stored in the database.
 function showTasks() {
@@ -10,7 +13,8 @@ function showTasks() {
         method: 'GET',
         url: '/list',
     }).then((response) => {
-        // Initializes a count variable to give each task a numeric value for ordering on the DOM
+        // Initializes a count variable to give each task a numeric value for ordering on the DOM. Did not
+        // use the id because this can get out of order when deleting from and adding tasks to the database.
         let count = 1;
         $('#toDoList').empty();
         for (task of response) {
@@ -29,6 +33,29 @@ function showTasks() {
             </tr>`);
             count++;
         }
+    }).catch((error) => {
+        alert(error);
+    });
+}
+// Function makes an ajax POST request to the back-end to add a new task to the database
+function addTask() {
+    let newTask = $('#newTask').val();
+    // Form validation which checks for empty inputs
+    if (newTask === '') {
+        alert('Please fill out the input form before submitting!');
+        return;
+    }
+    $.ajax({
+        method: 'POST',
+        url: '/list',
+        data: {
+            task: newTask
+        }
+    }).then(() => {
+        // Empties inputs on successful addition of task. 
+        // Calls the showTasks function to show all tasks on the DOM, including the recently added task.
+        $('#newTask').val('');
+        showTasks();
     }).catch((error) => {
         alert(error);
     });
